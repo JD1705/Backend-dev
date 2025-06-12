dbca2580ce841e651521f2a7ab0b827c990e40d4
jd1705 <soulbusterjd17@gmail.com>
2025-06-12 15:20:30 -0400

    style: restructuring of the project folder and files in it

--- app/databases/mongo_db.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-def get_db():
-    client = MongoClient(os.getenv("MONGO_URI"))
-    return client["backend_labs"]
