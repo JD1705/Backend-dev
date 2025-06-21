3200a6b05785bc54c5079110f2d5df1b0a73f1a8
jd1705 <soulbusterjd17@gmail.com>
2025-06-21 13:32:59 -0400

    new_feature: creation of different tests in test_main for endpoint "/sum/" in app/main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException,status,Query
+from typing import Annotated
 
 app = FastAPI()
 
@@ -8,4 +9,14 @@ def root():
 
 # basic CRUD functions
 
-@app.post('/sign-in/')
+@app.post('/sum/')
+async def sum_endpoint(a: Annotated[int,None] = Query(None), b: Annotated[int,None] = Query(None)):
+    if a == None or b == None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='all parameters must be passed to'
+        )
+    return {
+        'result':a + b,
+        'status_code':status.HTTP_200_OK
+    }
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
+from fastapi.testclient import TestClient
+from ..app.main import app
 
-
-
+client = TestClient(app)
 
 # basic tests
 
@@ -15,3 +16,46 @@ def test_sum_negative_numbers():
 
 def test_sum_zeros():
     assert sum(0, 0) == 0
+
+# practice with another function
+
+def multi(num_1: int, num_2: int):
+    return num_1 * num_2
+
+# test must begin with test_*
+
+def test_positive_numbers():
+    assert multi(2,3) == 6
+    
+def test_positive_and_zero():
+    assert multi(3, 0) == 0
+    
+def test_one_positive_and_negative():
+    assert multi(2,-3) == -6
+    
+# API tests require TestClient
+
+def test_status_code():
+    response = client.post('/sum/?a=4&b=2')
+    print(response.json())
+    assert response.status_code == 200
+    
+def test_sum_response():
+    response = client.post('/sum/?a=4&b=2')
+    assert response.json()['result'] == 6
+    
+def test_sum_plus_none_raise_exception():
+    response = client.post('/sum/?a=4')
+    print(response.json())
+    assert response.status_code == 400
+    assert response.json()['detail'] == 'all parameters must be passed to'
+    
+def test_if_parameters_are_none_raise_exception():
+    response = client.post('/sum/')
+    print(response.json())
+    assert response.status_code == 400
+    assert response.json()['detail'] == 'all parameters must be passed to'
+    
+def test_if_parameters_are_not_int_and_raise_exception():
+    response = client.post('/sum/?a=4&b=abc')
+    assert response.status_code == 422
